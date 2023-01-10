1511283754d685e5c4b7a797347e27fd77440e6f
ankitvat <ankitvat874@gmail.com>
2023-01-10 15:55:31 +0530

    added bird animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,34 @@
 import type { NextPage } from "next";
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import logo from "../assets/images/logo.png";
 import background from "../assets/images/back.png";
+import birdRight from "../assets/images/right-bird.png";
+import birdLeft from "../assets/images/left-bird.png";
+import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
+  const [isDesktop, setDesktop] = React.useState(false);
+
+  React.useEffect(() => {
+    if (window.innerWidth > 1430) {
+      setDesktop(true);
+    } else {
+      setDesktop(false);
+    }
+
+    const updateMedia = () => {
+      if (window.innerWidth > 1430) {
+        setDesktop(true);
+      } else {
+        setDesktop(false);
+      }
+    };
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-5 backz">
       <Head>
@@ -27,14 +51,68 @@ const Home: NextPage = () => {
           }}
         />
       </div>
+      {isDesktop ? (
+        <>
+          <motion.div
+            animate={{
+              x: 600,
+              y: -600,
+              transitionEnd: {
+                display: "none",
+              },
+            }}
+            transition={{ ease: "easeOut", duration: 14 }}
+            style={{
+              position: "absolute",
+              top: "30%",
+              left: 0,
+            }}
+          >
+            <Image
+              src={birdRight}
+              height={300}
+              priority
+              width={300}
+              quality={100}
+              alt="bird-img"
+            />
+          </motion.div>
+          <motion.div
+            animate={{
+              x: -400,
+              y: 700,
+              transitionEnd: {
+                display: "none",
+              },
+            }}
+            transition={{ ease: "easeOut", duration: 18 }}
+            style={{
+              position: "absolute",
+              top: "40%",
+              right: 0,
+            }}
+          >
+            <Image
+              src={birdLeft}
+              height={430}
+              priority
+              width={430}
+              quality={100}
+              alt="bird-img"
+            />
+          </motion.div>
+        </>
+      ) : null}
 
       <Image
         src={logo}
         alt="logo-image"
         className="logoImage"
+        priority
         height="300"
         width="400"
       />
+
       <h1 className="flex min-w-fit header">website under maintenance</h1>
       <h1 className="flex min-w-fit  header">we'll be back soon!</h1>
 
